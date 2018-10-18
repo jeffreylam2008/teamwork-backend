9258f2be05f6051dc900f1304a722171166a4b75
Jeffrey Lam <jeffreylam2008@gmail.com>
2018-10-18 23:42:48 +0800

    201810181142

--- index.php
+++ index.php
@@ -827,9 +827,10 @@ $app->group('/api/v1/systems/menu', function () {
             ["order" => 0, "id" => 32, "parent_id" => 65, "name" => "Create", "isParent" => "", "slug"=>"invoices/donew"],
             ["order" => 0, "id" => 22, "parent_id" => "", "name" => "Administration", "isParent" => "", "slug"=>""],
             ["order" => 0, "id" => 71, "parent_id" => 22, "name" => "Settings", "isParent" => "", "slug"=>"administration/settings"],
-            ["order" => 0, "id" => 32, "parent_id" => 22, "name" => "Shop", "isParent" => "", "slug"=>"administration/shops"],
+            ["order" => 0, "id" => 35, "parent_id" => 22, "name" => "Shop", "isParent" => "", "slug"=>"administration/shops"],
             ["order" => 0, "id" => 555, "parent_id" => 44, "name" => "User", "isParent" => "", "slug"=>""],
-            ["order" => 0, "id" => 6, "parent_id" => 22, "name" => "Employee", "isParent" => "", "slug"=>"administration/employees"],
+            ["order" => 0, "id" => 6, "parent_id" => 22, "name" => "Employees", "isParent" => "", "slug"=>"administration/employees"],
+            ["order" => 0, "id" => 33, "parent_id" => 4, "name" => "Quotations", "isParent" => "", "slug"=>"quotations"],
             ["order" => 0, "id" => 26, "parent_id" => 65, "name" => "List", "isParent" => "", "slug"=>"invoices/list"]
         ];
         return $response->withJson($data, 200);
@@ -862,10 +863,5 @@ $app->group('/api/v1/systems/shops', function () {
         return $response->withJson($callback, 200);
     });
 });
-/**
- * 
- */
-$app->group('', function() {
 
-});
 $app->run();
