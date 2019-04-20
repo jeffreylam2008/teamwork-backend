db96dbf1adf985f64c33c7ef7dbb1bb0c8be2171
Jeffrey Lam <jeffreylam2008@gmail.com>
2019-04-20 23:59:46 +0800

    201904201200

--- app/customers.php
+++ app/customers.php
@@ -63,4 +63,36 @@ $app->group('/api/v1/customers', function () {
     $this->post("/", function(Request $request, Response $response, array $args){
        
     });
+
+    /**
+	 * Categories PATCH Request
+	 * categories-patch
+	 * 
+	 * To update current record on DB
+	 */
+	$app->patch('/{cust_code}', function(Request $request, Response $response, array $args){
+		$err = [];
+		$_cust_code = $args['cust_code'];
+		$db = connect_db();
+		// POST Data here
+		$body = json_decode($request->getBody(), true);
+		$_now = date('Y-m-d H:i:s');
+		$db->beginTransaction();
+		$q = $db->prepare("
+            UPDATE `t_customers` SET 
+            `desc` = '".$body["i-desc"]."',
+            `modify_date` = '".$_now."'
+            WHERE `cate_code` = '".$_cust_code."';
+        ");
+		$q->execute();
+		// no fatch on update 
+		$err = $q->errorinfo();
+		$db->commit();
+		$callback = [
+			"query" => "",
+			"error" => ["code" => $err[0], "message" => $err[1]." ".$err[2]]
+		];
+		return $response->withJson($callback,200);
+		
+	});
 });
