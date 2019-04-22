6ec2dcee7498d4a0ac4b46775861f4ed132228bb
Jeffrey Lam <jeffreylam2008@gmail.com>
2019-04-22 17:35:51 +0800

    201904220535

--- app/customers.php
+++ app/customers.php
@@ -1,14 +1,14 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-$app->group('/api/v1/customers', function () {
+$app->group('/api/v1/customers', function () use($app) {
     /**
      * Customer GET Request
      * customer-get
      * 
      * To get all customer record
      */
-    $this->get('/', function(Request $request, Response $response, array $args) {
+    $app->get('/', function(Request $request, Response $response, array $args) {
         $err = "";
         $db = connect_db();
         $q = $db->prepare("select * from `t_customers`;");
@@ -33,7 +33,7 @@ $app->group('/api/v1/customers', function () {
      * 
      * To get single record based on the customer code
      */
-    $this->get('/{cust_code}', function(Request $request, Response $response, array $args){
+    $app->get('/{cust_code}', function(Request $request, Response $response, array $args){
         $_cust_code = $args['cust_code'];
         $err = "";
         $db = connect_db();
@@ -60,13 +60,13 @@ $app->group('/api/v1/customers', function () {
      * 
      * To create new record on customer table 
      */
-    $this->post("/", function(Request $request, Response $response, array $args){
+    $app->post("/", function(Request $request, Response $response, array $args){
        
     });
 
     /**
-	 * Categories PATCH Request
-	 * categories-patch
+	 * Customers PATCH Request
+	 * Customers-patch
 	 * 
 	 * To update current record on DB
 	 */
@@ -80,19 +80,34 @@ $app->group('/api/v1/customers', function () {
 		$db->beginTransaction();
 		$q = $db->prepare("
             UPDATE `t_customers` SET 
-            `desc` = '".$body["i-desc"]."',
+            `mail_addr` = '".$body["i-mail_addr"]."',
+            `shop_addr` = '".$body["i-shop_addr"]."',
+            `delivery_addr` = '".$body["i-delivery_addr"]."',
+            `attn_1` = '".$body["i-attn_1"]."',
+            `phone_1` = '".$body["i-phone_1"]."',
+            `email_1` = '".$body["i-email_1"]."',
+            `attn_2` = '".$body["i-attn_2"]."',
+            `phone_2` = '".$body["i-phone_2"]."',
+            `fax_1` = '".$body["i-fax_1"]."',
+            `fax_2` = '".$body["i-fax_2"]."',
+            `email_2` = '".$body["i-email_2"]."',
+            `statement_remark` = '".$body["i-statement_remark"]."',
+            `name` = '".$body["i-name"]."',
+            `group_name` = '".$body["i-group_name"]."',
+            `pm_code` = '".$body["i-pm_code"]."',
+            `pt_code` = '".$body["i-pt_code"]."',
             `modify_date` = '".$_now."'
-            WHERE `cate_code` = '".$_cust_code."';
+            WHERE `cust_code` = '".$_cust_code."';
         ");
 		$q->execute();
 		// no fatch on update 
 		$err = $q->errorinfo();
 		$db->commit();
 		$callback = [
-			"query" => "",
+			"query" => "", 
 			"error" => ["code" => $err[0], "message" => $err[1]." ".$err[2]]
 		];
-		return $response->withJson($callback,200);
+		return $response->withJson($callback, 200);
 		
 	});
 });
