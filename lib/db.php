0a20de7e712288ace3757f0d01139dd149bd167f
Jeffrey Lam <jeffreylam2008@gmail.com>
2019-05-19 11:58:23 +0800

    201905191158

--- lib/db.php
+++ lib/db.php
@@ -1,21 +1,29 @@
 <?php
-function connect_db() 
+Class Database
 {
-	$dsn = 'mysql:dbname=teamwork;host=localhost;charset=utf8';
-	$user = 'dbadmin';
-	$pass = 'dbadmin';
+	var $connection;
+	public function connect_db() 
+	{
+		$dsn = 'mysql:dbname=teamwork;host=localhost;charset=utf8';
+		$user = 'dbadmin';
+		$pass = 'dbadmin';
 
-	//mysql_set_charset('utf8',$connection);
-	//$connection->set_charset("utf8");
-	/* check connection */
-	try {
-		$connection = new PDO($dsn, $user, $pass);
-		$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-		//$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-	} catch (PDOException $e) {
-		echo 'Connection failed: ' . $e->getMessage();
+		//mysql_set_charset('utf8',$connection);
+		//$connection->set_charset("utf8");
+		/* check connection */
+		try {
+			$this->connection = new PDO($dsn, $user, $pass);
+			$this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+			//$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		} catch (PDOException $e) {
+			echo 'Connection failed: ' . $e->getMessage();
+		}
+		
+		return $this->connection;
 	}
-	
-	return $connection;
+	public function disconnect_db()
+	{
+		$this->connection = null;
+	}
 }
