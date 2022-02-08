2d970bfeeb07751511de962e0a7ebc16cae66f0f
jeffreylam2008 <jeffreylam2008@gmail.com>
2022-02-08 20:43:56 +0800

    Update invoices.php

--- app/invoices.php
+++ app/invoices.php
@@ -306,7 +306,9 @@ $app->group('/api/v1/inventory/invoices', function () {
 
         //disconnection DB
         
-        return $response->withJson($_callback, 200);
+        return $response->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withJson($_callback, 200);
     });
 
     /**
